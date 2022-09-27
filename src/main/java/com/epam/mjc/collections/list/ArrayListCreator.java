44f5f149b8d7691b6d620963397f2c9899fd60af
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-09-27 06:27:14 +0500

    Solve the ArrayListCreator

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -4,6 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ArrayListCreator {
+    public static void main(String[] args) {
+        List<String> sourceList = new ArrayList<>(List.of("I", "love", "you"));
+        ArrayListCreator arrayListCreator = new ArrayListCreator();
+        arrayListCreator.createArrayList(sourceList);
+    }
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList<String> result = new ArrayList<>();
+        if (sourceList.size() < 3) {
+            return new ArrayList<>();
+        } else {
+            for (int i = 0; i < sourceList.size(); i++) {
+                 if ((i+1) % 3 == 0) {
+                     result.add(sourceList.get(i));
+                     result.add(sourceList.get(i));
+                 }
+            }
+        }
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -1,9 +1,11 @@
 package com.epam.mjc.collections.list;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        return new LinkedList<>();
     }
 }
